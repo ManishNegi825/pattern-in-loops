81de9f1d9a6a84322a2af132c235f52f1aa7cf90
ManishNegi825 <108972949+ManishNegi825@users.noreply.github.com>
2022-07-25 12:34:15 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=7;i++)
+    {
+        k='A';
+        for(j=1;j<=13;j++)
+        {
+            if(j<=8-i || j>=6+i || i==1)
+     {
+                printf("%c",k);
+                j<7?k++:k--;
+     }
+                else
+            {
+                printf(" ");
+                j<7?k++:k--;
+            }
+        }
+        printf("\n");
+    }
+        return 0;
+}
